65b9af23ac987cc288af2a4561b3e3d583b8de45
Rodriques <rodriques.perry@gmail.com>
2025-12-28 15:11:54 -0600

    Initial server commit"

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def portfolio():
+    return render_template('./index.html')
+
+
+@app.route("/generic")
+def generic():
+    return render_template('./generic.html')
+
+
+@app.route("/elements")
+def elements():
+    return render_template('./elements.html')
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
