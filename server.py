8c62a56dc4d91ba4b2e830251256a747df17d1f4
Rodriques <rodriques.perry@gmail.com>
2025-12-28 16:46:53 -0600

    set up pseudo-db and wrote to it from the portfolio page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -7,14 +7,29 @@ def portfolio():
     return render_template('./index.html')
 
 
-@app.route("/generic")
-def generic():
-    return render_template('./generic.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name + '.html')
 
 
-@app.route("/elements")
-def elements():
-    return render_template('./elements.html')
+def write_to_db(data):
+    with open('database.txt', mode='a') as database:
+          database.write(f'{str(data)}\n')
+    
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        # Transform whatever the data is into a dictionary
+        try:
+          data = request.form.to_dict()
+          write_to_db(data)
+          return redirect('/generic')
+        except:
+            return 'Did not save to db!'
+    else:
+        return 'something went wrong. Try again!'
+    
 
 
 if __name__ == "__main__":
